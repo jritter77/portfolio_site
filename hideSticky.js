d110418aac9eafe40494b7db8f53e95af708f86d
Jake Ritter <jakeritter77@gmail.com>
2022-02-17 15:07:36 -0800

    - adjusted navbar trigger

--- hideSticky.js
+++ hideSticky.js
@@ -2,7 +2,7 @@ const about = document.getElementById("about");
 const nav = document.getElementById("navbar");
 
 function checkTop() {
-  if (about.getBoundingClientRect().top <= 0) {
+  if (about.getBoundingClientRect().top <= 1) {
     nav.classList.add("isStuck");
   } else {
     nav.classList.remove("isStuck");
